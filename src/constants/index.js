c0f20e2349c03ef43abb73fe4312fff115b39028
Victor Busayor <ogunrinolavictor2015@gmail.com>
2024-02-24 22:16:05 +0100

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,242 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+
+import {
+    car,
+    contact,
+    css,
+    vue,
+    estate,
+    git,
+    github,
+    html,
+    javascript,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    linkedin,
+    snapgram,
+    summiz,
+    threads,
+    twitterx,
+    tailwindcss,
+    whatsapp,
+    typescript
+    
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: vue,
+        name: "Vue",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const socialLinks = [
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Busayor2020',
+    },
+    {
+        name: 'WhatsApp',
+        iconUrl: whatsapp,
+        link: 'https://wa.me/2347017840486',
+    },
+    {
+        name: 'Twitter-X',
+        iconUrl: twitterx,
+        link: 'https://twitter.com/BusaryoR19',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/ogunrinola-victor-8358bb198/',
+    }
+];
+
+export const experiences = [
+    {
+        title: "Front-End Developer",
+        company_name: "LiquidEdge Computers",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    /*
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    */
+    {
+        title: "Shopify Developer",
+        company_name: "Xyte Agency",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Vue.Js Developer",
+        company_name: "Ogilivista",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - September 2023",
+        points: [
+            "Developing and maintaining web applications using Vue.Js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const projects = [
+
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise and informative summaries from lengthy text content or blogs.',
+        link: 'http://probable-paste.surge.sh/',
+    },
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Language Translator',
+        description: 'I developed a web application that transcribes voice recordings into text and translates them into other languages.',
+        link: 'https://smart-cough.surge.sh/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Responsive E-commerce website',
+        description: 'Built a responsive Front-End e-commerce website.',
+        link: 'http://hesitant-balance.surge.sh/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Behance Clone (Portfolio)',
+        description: 'Built a complete clone of Behance, allowing users to share their work and designs using their Portfolio.',
+        link: 'https://subsequent-dress.surge.sh/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'A full-stack e-commerce Web Application',
+        description: 'Developed a full-stack e-commerce web application, with payloadCMS and integrated Stripe for payment gateway.',
+        link: 'https://e-commerce-busayo.payloadcms.app/',
+    }
+
+     /*
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    }*/
+];
